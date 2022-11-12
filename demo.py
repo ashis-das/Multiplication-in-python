a893fc3ac1e4e38e31d67465cbbf2eb44d9e189e
Ashis Das <105511844+ashis-das@users.noreply.github.com>
2022-11-13 00:13:50 +0530

    Add files via upload

---
+++ demo.py
@@ -0,0 +1,5 @@
+# Multiplication of two numbers
+#take two user input number and mulitply them
+num1=int(input("enter 1st integer no."))
+num2=int(input("enter 2nd integer no."))
+print("the Multiplication of",num1 ,"and",num2 ,"are",num1*num2)
